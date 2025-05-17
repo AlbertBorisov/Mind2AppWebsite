66d14039144f6a41264cbe4ad78f17e155dd9247
AlbertBorisov <borisovalbert722@gmail.com>
2025-05-18 00:37:07 +0200

    auto: Änderungen übernommen

--- autopush_loop.sh
+++ autopush_loop.sh
@@ -2,7 +2,7 @@
 
 while true
 do
-  ./autopush.sh
-  echo "Änderungen gepusht am $(date)"
-  sleep 300
+    bash /c/Users/boris/PycharmProjects/Mind2App/autopush.sh
+    echo "Änderungen gepusht am $(date)"
+    sleep 300  # 300 Sekunden = 5 Minuten
 done
